37c2a458823b40812c82310e113472ea342b3084
Luke Todd <luke_todd9@hotmail.com>
2025-03-14 17:17:30 -0600

    Update db.py

--- src/lib/db.py
+++ src/lib/db.py
@@ -2,6 +2,13 @@ from tinydb import TinyDB, Query
 import couchdb
 
 class CouchEngine:
+    
+    COUCHDB_USER = "myadmin"
+COUCHDB_PASSWORD = "mypassword"
+COUCHDB_URL = f"http://{COUCHDB_USER}:{COUCHDB_PASSWORD}@localhost:5984/"
+server = couchdb.Server(COUCHDB_URL)
+
+
 def insert(self):
   server = couchdb.Server("http://admin:password@localhost:5984/")
   db = server.create("mydatabase")  # Creates a new database
